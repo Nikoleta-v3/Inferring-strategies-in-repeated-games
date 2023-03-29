1409cc1a14d34b90d8d84a3d7e0463f566b0963c
Nikoleta-v3 <glynatsi@evolbio.mpg.de>
2023-03-29 15:50:53 +0200

    adds requirement txt and functions used for bayesian update

---
+++ src/bayesian.py
@@ -0,0 +1,40 @@
+def init_prior_uniform(n):
+    """
+    Returns 1 / n where n is the number of possibles strategies.
+
+    This is known as the prior.
+
+    If we are fitting for n = 3, then the initial prior that a player
+    is playing any of the 3 strategies is 1 / 3.
+    """
+    return 1 / n
+
+
+def normalising_constant(priors, likelihoods):
+    return sum([p * l for p, l in zip(priors, likelihoods)])
+
+
+def posterior(self_prior,
+              self_likelihood,
+              other_priors,
+              other_likelihoods):
+
+    return (self_prior * self_likelihood) / normalising_constant(
+        [self_prior] + other_priors, [self_likelihood] + other_likelihoods
+    )
+
+
+class MemoryOne:
+    def __init__(self, states_action_dict, error=0):
+        self.error = error
+        self.states_action_dict = states_action_dict
+
+    def likelihood(self, action, history):
+
+        if (
+            action
+            == self.states_action_dict[history]
+        ):
+            return 1 * (1 - self.error)
+        else:
+            return self.error
