ed7b9a4082c090725b9e4a0d5062dbae901e0807
Nikoleta-v3 <glynatsi@evolbio.mpg.de>
2023-04-21 11:56:29 +0200

    seperates the code for stationary distributions and for estimating br

--- src/stationary.py
+++ src/stationary.py
@@ -1,9 +1,7 @@
-import csv
-import itertools
-import sys
-
+"""A file that contains the source code necessary for calculating stationary
+distributions.
+"""
 import sympy as sym
-import tqdm
 
 
 def matrix(player, opponent):
@@ -132,48 +130,3 @@ def stationary_no_discount(player, coplayer, epsilon):
     )
 
     return v_vector
-
-
-if __name__ == "__main__":
-    player_idx = int(sys.argv[1])
-
-    p0, pcc, pcd, pdc, pdd = sym.symbols(
-        "p_{0}, p_{CC}, p_{CD}, p_{DC}, p_{DD}"
-    )
-
-    delta = sym.symbols("delta")
-
-    epsilon = sym.symbols("epsilon")
-
-    b, c = 1, sym.symbols("c")
-
-    pure_strategies = list(itertools.product([0, 1], repeat=5))
-
-    labels = [f"d{i}" for i, _ in enumerate(pure_strategies)]
-
-    labels[0] = "ALLD (D)"
-    labels[16] = "ALLD (C)"
-
-    labels[15] = "ALLC (D)"
-    labels[31] = "ALLC (C)"
-
-    labels[8] = "GT (D)"
-    labels[24] = "GT (C)"
-
-    labels[9] = "WSLS (D)"
-    labels[25] = "WSLS (C)"
-
-    labels[10] = "TFT (D)"
-    labels[26] = "TFT (C)"
-    player = pure_strategies[player_idx]
-
-    payoffs = []
-
-    for coplayer in tqdm.tqdm(pure_strategies):
-        ss = stationary(coplayer, player, epsilon, delta)
-
-        payoffs.append(sum(ss @ sym.Matrix([b - c, -c, b, 0])).simplify())
-
-    with open(f"outputs/{labels[player_idx]}_payoffs.txt", "w") as f:
-        writer = csv.writer(f, delimiter="\t")
-        writer.writerows(zip(labels, payoffs))
