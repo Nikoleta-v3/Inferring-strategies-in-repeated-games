2462c4b2a64e671569f579290bee1f14031b5b1e
Yohsuke Murase <yohsuke.murase@gmail.com>
2023-05-16 15:31:41 +0200

    [WIP] implementing mockup

--- src/mockup.py
+++ src/mockup.py
@@ -3,6 +3,7 @@ import itertools
 import bayesian
 import axelrod as axl
 
+#%%
 from axelrod.action import Action
 
 C, D = Action.C, Action.D
@@ -18,8 +19,9 @@ epsilon = PARAMETERS["epsilon"]
 seq_size = PARAMETERS["seq_size"]
 action_map = {1: C, 0: D}
 
+# %%
 
-class BeyesianBestResponseStrategy:
+class BayesianBestResponseStrategy:
     def __init__(self, initial_sequence):
         self.initial_sequence = initial_sequence
 
@@ -77,26 +79,35 @@ def long_term_payoffs(
     """Compute the long term payoffs of the strategy against the opponent."""
     # [TODO] implement NG
 
+# %%
+initial_sequences = itertools.product([C, D], repeat=seq_size)
+list(initial_sequences)
+
+# %%
 
 if __name__ == "__main__":
 
     initial_sequences = itertools.product([C, D], repeat=seq_size)
 
     for init_seq in initial_sequences:
-        s = BayesianBestResponseStrategy(init_seq)
+        # make string from list
+        init_seq_str = "".join([a.name for a in init_seq])
+        s = axl.Cycler(init_seq_str)
 
         pure_memory_one_strategies = itertools.product([0, 1], repeat=5)
         total_payoff = 0
+        #s = BayesianBestResponseStrategy(init_seq)
         for i in pure_memory_one_strategies:
-            opponent = axl.MemoryOneStrategy(i[1:], initial=action_map[i[0]])
+            opponent = axl.MemoryOnePlayer(i[1:], initial=action_map[i[0]])
 
             # simulating game
-            match = axl.match(s, opponent, turns=len(init_seq))
+            match = axl.Match(players=(s, opponent), turns=len(init_seq))
             _ = match.play()
             history = match.result
             opening_payoffs = match.final_score_per_turn()
 
             # inferring co-player and best response
+            bs, exp_p = infer_best_response_and_expected_payoffs(history)
             bs, exp_p = s.best_response_and_expected_payoffs(history)
 
             lt_payoffs = long_term_payoffs(
@@ -105,3 +116,22 @@ if __name__ == "__main__":
             total_payoff += lt_payoffs[0]
 
         print(f"{init_seq} {total_payoff}")
+
+# %%
+init_seq = [C,D,C]
+init_seq_str = "".join([a.name for a in init_seq])
+axl.Cycler(init_seq_str)
+
+
+# %%
+pure_memory_one_strategies = itertools.product([0, 1], repeat=5)
+i = list(pure_memory_one_strategies)[10]
+#for i in pure_memory_one_strategies:
+opponent = axl.MemoryOnePlayer(i[1:], initial=action_map[i[0]]) # %%
+opponent
+
+# %%
+
+def infer_best_response_and_expected_payoffs(history):
+
+    return bs, exp_p
---
+++ src/note.md
@@ -0,0 +1,11 @@
+# procedure
+
+Based on a given inital sequences, we calculate the posterior distribution.
+
+- The posterior distribution: $p(i)$, where $i$ is the index of the strategy $[1,32]$
+- $\pi(i, j)$: the long-term payoff of strategy $i$ against strategy $j$
+
+If the focal player takes strategy $1$, for instance, the expected long-term payoff $P(1) = $\sum_i \pi(1,i) p(i)$.
+In general, $P(j) = \sum_i \pi(j,i) p(i)$.
+We want to find the strategy $j$ that maximizes $P(j)$.
+$j = \argmax P(j)$.
