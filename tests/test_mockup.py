90320730d9f4bd5d194ac67500a8a4e6f5656c66
Nikoleta-v3 <glynatsi@evolbio.mpg.de>
2023-05-17 11:04:52 +0200

    uncomment out test

--- tests/test_mockup.py
+++ tests/test_mockup.py
@@ -10,26 +10,26 @@ import mockup as mcu
 
 import numpy as np
 
-# def test_calculate_payoff_matrix():
-#     benefit, cost, delta, epsilon = 2, 1, .999, 0
+def test_calculate_payoff_matrix():
+    benefit, cost, delta, epsilon = 2, 1, .999, 0
 
-#     matrix = mcu.calculate_payoff_matrix(benefit, cost, delta, epsilon)
+    matrix = mcu.calculate_payoff_matrix(benefit, cost, delta, epsilon)
 
-#     # Dimension
-#     assert matrix.shape == (32, 32)
+    # Dimension
+    assert matrix.shape == (32, 32)
 
-#     # ALLD
-#     assert np.isclose(matrix[0, 0], 0)
-#     assert np.isclose(matrix[0, 31], 2)
+    # ALLD
+    assert np.isclose(matrix[0, 0], 0)
+    assert np.isclose(matrix[0, 31], 2)
 
-#     # ALLC
-#     assert np.isclose(matrix[31, 0], -1)
-#     assert np.isclose(matrix[31, 26], 1) # TFT (C)
-#     assert np.isclose(matrix[31, 10], 0.998) # TFT (D)
+    # ALLC
+    assert np.isclose(matrix[31, 0], -1)
+    assert np.isclose(matrix[31, 26], 1) # TFT (C)
+    assert np.isclose(matrix[31, 10], 0.998) # TFT (D)
 
-#     # TFT
-#     assert np.isclose(matrix[26, 26], 1)
-#     assert np.isclose(matrix[26, 25], 1) # WSLS
+    # TFT
+    assert np.isclose(matrix[26, 26], 1)
+    assert np.isclose(matrix[26, 25], 1) # WSLS
 
 
 def test_posterior_distribution_TFT_C():
